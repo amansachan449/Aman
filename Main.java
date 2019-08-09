43b8b5cd0b4493ef66ea3e01e9f767cf7be064ba
amansachan449 <53946210+amansachan449@users.noreply.github.com>
2019-08-09 20:54:08 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main{
+           public static void main(String args[]){
+                       Scanner sc=new Scanner(system.in);
+                       int X= sc.nextInt();
+                       int P= sc.nextInt();
+                       int sum=0;
+                       do{
+                         sum+=x;
+                         x=x-((int)(x*(p/100)));
+                       }while(x!=0);
+                       System.out.println(sum);
+            }
+}
